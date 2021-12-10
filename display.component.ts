ec86022826004847b998548348e2db192d658a80
prafulkumar9620 <64548472+prafulkumar9620@users.noreply.github.com>
2021-12-10 13:05:24 +0530

    Add files via upload

---
+++ display.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CommentService } from '../comment.service';
+import { Comments } from '../Comments';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.css']
+})
+export class DisplayComponent implements OnInit {
+
+  constructor(private service:CommentService) { }
+
+  comments: Comments[]=[
+    {'cid':9001,'commenter':'sunil','pid':1001,'comment':'your notes is so simple'},
+    {'cid':9002,'commenter':'kishore','pid':1001,'comment':'please give elaborated discussion'},
+    {'cid':9003,'commenter':'sunil','pid':1002,'comment':'most time waste topic for india'},
+    {'cid':9004,'commenter':'kishore','pid':1002,'comment':'Teenages must have good knowledge on politics'},
+    {'cid':9005,'commenter':'Anjali','pid':1004,'comment':'Can you explain more on this'}
+
+  ];
+  tcomments:any;
+
+  ngOnInit(): void {
+  }
+  getComments(){
+    this.service.getAllComments()
+    .subscribe( response =>{
+      this.tcomments=response;
+      console.log(response);
+    });
+  
+  }
+
+}
---
+++ register.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CommentService } from '../comment.service';
+import { Comments } from '../Comments';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  cid=9001;
+  commenter='sunil';
+  pid=1001;
+  comment=' your notes is so simple';
+  
+
+  constructor(private service: CommentService) { }
+
+  ngOnInit(): void {
+   
+
+  }
+  
+
+  postComment(){
+    let comment : Comments = {
+      "cid":this.cid,
+      "commenter":this.commenter,
+      "pid":this.pid,
+      "comment":this.comment
+    }
+    
+    this.service.postComment(comment);
+    // write the code for the springboot connectivity
+    console.log(comment.comment);
+    
+  }
+  }
